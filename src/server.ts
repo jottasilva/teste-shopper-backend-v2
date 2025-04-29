54a957db8caa3f7eb47ed801540ae0221da855c3
Jefferson Silva <jefferson_jsp@hotmail.com>
2025-04-29 18:12:52 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 config();
 import app from './app';
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env.PORT) || 80;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando na Porta ${PORT} em todas as interfaces`);
 });
